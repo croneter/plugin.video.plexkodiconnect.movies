b14b571dfeec83df814a6429ba54c87d7e22a6cc
croneter <croneter@users.noreply.github.com>
2019-05-29 19:14:58 +0200

    Hopefully fix Kodi crashing on playback startup

--- default.py
+++ default.py
@@ -46,7 +46,7 @@ def play():
         LOG.error('Error encountered, aborting')
         xbmcplugin.setResolvedUrl(HANDLE, False, xbmcgui.ListItem())
     elif result is True:
-        pass
+        xbmcplugin.setResolvedUrl(HANDLE, False, xbmcgui.ListItem())
     else:
         xbmcplugin.setResolvedUrl(HANDLE, True, result)
 
